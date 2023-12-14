1ea8e55878dc57ec5fc63c5ac52865a39e972706
Akshay Bodla <akshaybodla21@gmail.com>
2023-12-13 18:28:33 -0800

    Enabled deleting

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -11,6 +11,7 @@ import {
     Scripts,
     ScrollRestoration,
     useLoaderData,
+    useNavigation,
 } from '@remix-run/react';
 
 import type { LinksFunction } from '@remix-run/node';
@@ -31,6 +32,7 @@ export const loader = async () => {
 
 export default function App() {
     const { contacts } = useLoaderData<typeof loader>();
+    const navigation = useNavigation();
 
     return (
         <html lang='en'>
@@ -91,7 +93,9 @@ export default function App() {
                         )}
                     </nav>
                 </div>
-                <div id='detail'>
+                <div
+                    className={navigation.state === 'loading' ? 'loading' : ''}
+                    id='detail'>
                     <Outlet />
                 </div>
 
